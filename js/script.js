30d34458d74161c1b5d47e0232d8c32983295074
madtyrex <madtyrex@gmail.com>
2015-05-16 21:46:09 -0400

    Working basic map, with front end form and title

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#logos").click(function () {
+            alert("Ok");
+    });
+
+
+    function initialize() {
+    var mapOptions = {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 4
+};
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+});
+
